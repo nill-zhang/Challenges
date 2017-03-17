e39dc66e7cdd4394b14ccc7f3bc6c31106623be1
nill-zhang <andy.ledway3@gmail.com>
2017-03-16 23:36:27 -0400

    added queue data structure file

---
+++ queue.go
@@ -0,0 +1,36 @@
+package main
+
+// Add adds an item to the end of the list.
+func Add(){
+
+
+
+}
+// Remove removes the first item in the list.
+func Remove(){
+
+
+
+
+}
+
+
+// Peek returns the top of the queue.
+func Peek(){
+
+
+
+	
+}
+// IsEmpty returns true if and only if the queue is empty.
+func IsEmpty(){
+
+
+
+
+}
+
+func main() {
+
+}
+
