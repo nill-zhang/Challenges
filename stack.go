c90d7257014175f74031f033bda680474bb16c67
nill-zhang <andy.ledway3@gmail.com>
2017-03-16 23:36:56 -0400

    adding stack data structure file

---
+++ stack.go
@@ -0,0 +1,86 @@
+package main
+
+
+// Pop removes the top item from the stack
+func Pop(){
+
+
+
+
+}
+
+// Push add an item to the top of the stack.
+func Push(){
+
+
+
+
+}
+
+// Min return the minimum element in the stack
+func Min(){
+
+
+
+
+}
+
+// Len returns the length of the statck
+func Len(){
+
+
+
+
+}
+
+// Peek return the top of the stack
+func Peek(){
+
+
+
+
+}
+
+// isEmpty returns true if and only if the stack is empty
+func IsEmpty(){
+
+
+
+
+}
+
+//Three in One: Describe how you could use a single array to implement three stacks.
+//Hints: #2, #72, #38, #58
+
+func ThreeInOne(){
+
+
+
+
+
+}
+
+//Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+//Therefore, in real life, we would likely start a new stack when the previous stack exceeds some
+//threshold. Implement a data structure SetOfStacks that mimics this. SetO-fStacks should be
+//composed of several stacks and should create a new stack once the previous one exceeds capacity.
+//SetOfStacks. push() and SetOfStacks. pop() should behave identically to a single stack
+//(that is, pop () should return the same values as it would if there were just a single stack).
+//FOLLOW UP
+//Implement a function popAt ( int index) which performs a pop operation on a specific sub-stack.
+
+//Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.
+
+
+
+//Sort Stack: Write a program to sort a stack such that the smallest items are on the top. You can use
+//an additional temporary stack, but you may not copy the elements into any other data structure
+//(such as an array). The stack supports the following operations: push, pop, peek, and is Empty.
+
+
+//Animal Shelter: An animal shelter, which holds only dogs and cats, operates on a strictly"first in, first
+//out" basis. People must adopt either the "oldest" (based on arrival time) of all animals at the shelter,
+//or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of
+//that type). They cannot select which specific animal they would like. Create the data structures to
+//maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog,
+//and dequeueCat. You may use the built-in Linked list data structure.
